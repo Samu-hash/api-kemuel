bc38e5dcccaa76d85b48f8fefbe9c3bd720945f5
Samuel Alvarado <samu512br@gmail.com>
2024-01-28 12:41:55 -0600

    2024-01-28 modulo de encriptador
    2024-01-28 modulo de encriptador

---
+++ src/v1/routes/encriptadorRoutes.js
@@ -0,0 +1,14 @@
+const express = require('express')
+
+const router = express.Router()
+const encdec = require('../../controller/encriptadorController')
+
+router.post('/encriptador/encriptar', (req, res) => {
+
+})
+
+router.post = ('/encriptador/desencriptar', (req, res) => {
+
+})
+
+module.exports = router
--- src/v1/routes/index.js
+++ src/v1/routes/index.js
@@ -1,9 +1,11 @@
 const express = require('express')
 const router = express.Router()
 const u = require('./usuariosRoutes')
+const r = require('./encriptadorRoutes')
 
 const PATH_V1 = '/api/v1/'
 
 router.use(PATH_V1, u)
+router.use(PATH_V1, r)
 
 module.exports = router
